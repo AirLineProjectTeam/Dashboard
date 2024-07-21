258fb4d7e9a0ac86edbb74ad6f658abe755e02f8
TasnimZaid <tasnim.aboarqob@gmail.com>
2024-07-21 17:39:01 +0300

    dash chart

--- dashboard/src/component/pages/dashboard/MainpageDashoard/MainDashboard.jsx
+++ dashboard/src/component/pages/dashboard/MainpageDashoard/MainDashboard.jsx
@@ -7,6 +7,7 @@ ChartJS.register(CategoryScale, LinearScale, BarElement, ArcElement, Title, Tool
 
 function MainDashboard() {
   const [users, setUsers] = useState([]);
+  const [trips, setTrips] = useState([]);
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -26,7 +27,23 @@ function MainDashboard() {
       }
     };
 
+    const fetchTrips = async () => {
+      try {
+        const response = await axios.get('https://airline-tickets-46241-default-rtdb.firebaseio.com/trips/Trips.json');
+        if (response.data) {
+          const fetchedTrips = Object.keys(response.data).map(key => ({
+            id: key,
+            ...response.data[key]
+          }));
+          setTrips(fetchedTrips);
+        }
+      } catch (error) {
+        console.error('Error fetching trips:', error);
+      }
+    };
+
     fetchUsers();
+    fetchTrips();
   }, []);
 
   const couponData = users.reduce((acc, user) => {
@@ -53,7 +70,7 @@ function MainDashboard() {
       {
         label: '# of Users with Coupon',
         data: couponCounts,
-        backgroundColor: 'rgba(54, 162, 235, 0.8)', // Blue color for active users
+        backgroundColor: 'rgba(54, 162, 235, 0.8)',
         borderColor: 'rgba(54, 162, 235, 1)',
         borderWidth: 1,
       },
@@ -67,28 +84,52 @@ function MainDashboard() {
         label: '# of Users by Status',
         data: statusCounts,
         backgroundColor: [
-          'rgba(54, 162, 235, 0.7)', // Blue color for active users
-          'rgba(255, 99, 132, 0.7)', // Red color for deactivated users
+          'rgba(54, 162, 235, 0.7)',
+          'rgba(255, 99, 132, 0.7)',
         ],
         borderColor: [
-          'rgba(54, 162, 235, 6)',
+          'rgba(54, 162, 235, 1)',
           'rgba(255, 99, 132, 1)',
         ],
         borderWidth: 1,
       },
     ],
   };
-  
+
+  const destinationData = trips.reduce((acc, trip) => {
+    const destination = trip.destination || 'No Destination';
+    acc[destination] = (acc[destination] || 0) + 1;
+    return acc;
+  }, {});
+
+  const destinationLabels = Object.keys(destinationData);
+  const destinationCounts = Object.values(destinationData);
+
+  const destinationChartData = {
+    labels: destinationLabels,
+    datasets: [
+      {
+        label: '# of Trips by Destination',
+        data: destinationCounts,
+        backgroundColor: 'rgba(255, 99, 132, 0.8)', // Red color for destinations
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1,
+      },
+    ],
+  };
 
   return (
-    <div className="relative overflow-x-auto sm:rounded-lg ml-20 mt-10 flex w-[50%] ">
-    <div className="chart-container w-1/2 m-20">
-      <Bar data={couponChartData} />
+    <div className="relative overflow-x-auto sm:rounded-lg ml-20 mt-10 flex w-[50%]">
+      <div className="chart-container w-1/3 m-5">
+        <Bar data={couponChartData} />
+      </div>
+      <div className="chart-container w-1/3 m-5">
+        <Pie data={statusChartData} />
+      </div>
+      <div className="chart-container w-1/3 m-5">
+        <Bar data={destinationChartData} />
+      </div>
     </div>
-    <div className="chart-container w-1/2">
-      <Pie data={statusChartData} />
-    </div>
-  </div>
   );
 }
 
